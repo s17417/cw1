2b144c5ce3e31c73e41c2d9ea62698e54b882062
s17417 <s17417@pjwstk.edu.pl>
2020-02-29 16:30:41 +0100

    zmiany

--- Cw1ConsoleApp/Cw1ConsoleApp/Program.cs
+++ Cw1ConsoleApp/Cw1ConsoleApp/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+      
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = args.Length > 0 ? args[0] : "http://www.pja.edu.pl";
+            //var httpClient = new HttpClient();   //to jest to samo co HttpClient httpClient
+            var client = new HttpClient();
+            var result= await client.GetAsync(url);
+            
+            if (result.IsSuccessStatusCode)
+            {
+                string htmlContent = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(htmlContent);
+
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+
+            }
+
         }
     }
 }
